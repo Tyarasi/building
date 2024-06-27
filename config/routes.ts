6d01a06e3cab7b22858beee5ed7e5547ae456958
Teguh <teguhamanah8@gmail.com>
2024-06-27 18:53:54 +0700

    Add Dashboard V.1.0.0 not Fix

--- config/routes.ts
+++ config/routes.ts
@@ -11,38 +11,38 @@
  * @doc https://umijs.org/docs/guides/routes
  */
 export default [
-  {
-    path: '/user',
-    layout: false,
-    routes: [
-      {
-        path: '/user/login',
-        layout: false,
-        name: 'login',
-        component: './user/login',
-      },
-      {
-        path: '/user',
-        redirect: '/user/login',
-      },
-      {
-        name: 'register-result',
-        icon: 'smile',
-        path: '/user/register-result',
-        component: './user/register-result',
-      },
-      {
-        name: 'register',
-        icon: 'smile',
-        path: '/user/register',
-        component: './user/register',
-      },
-      {
-        component: '404',
-        path: '/*',
-      },
-    ],
-  },
+  // {
+  //   path: '/user',
+  //   layout: false,
+  //   routes: [
+  //     {
+  //       path: '/user/login',
+  //       layout: false,
+  //       name: 'login',
+  //       component: './user/login',
+  //     },
+  //     {
+  //       path: '/user',
+  //       redirect: '/user/login',
+  //     },
+  //     {
+  //       name: 'register-result',
+  //       icon: 'smile',
+  //       path: '/user/register-result',
+  //       component: './user/register-result',
+  //     },
+  //     {
+  //       name: 'register',
+  //       icon: 'smile',
+  //       path: '/user/register',
+  //       component: './user/register',
+  //     },
+  //     {
+  //       component: '404',
+  //       path: '/*',
+  //     },
+  //   ],
+  // },
   {
     path: '/dashboard',
     name: 'dashboard',
@@ -57,6 +57,18 @@ export default [
         icon: 'smile',
         path: '/dashboard/analysis',
         component: './dashboard/analysis',
+      },
+      {
+        name: 'monitor',
+        icon: 'smile',
+        path: '/dashboard/monitor',
+        component: './dashboard/monitor',
+      },
+      {
+        name: 'workplace',
+        icon: 'smile',
+        path: '/dashboard/workplace',
+        component: './dashboard/workplace',
       },
     ],
   },
